90413d1fd9c798bfd6c83ae30454aa9cdfc9de8c
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-02 05:12:20 +0000

    done the code 2

---
+++ p2original.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int cmp(int a,int b, int c)
+{
+  if(a>b)
+  {
+    if(a>c)
+    {
+      return a;
+    }
+    else
+    {
+      return c;
+    }
+
+  }
+  else
+  {
+    if(b>c)
+    {
+      return b;
+
+    }
+    else
+    {
+      return c;
+    }
+  }
+}    
+ int input()
+ {
+   int x;
+   printf("enter the number\n");
+   scanf("%d",&x);
+  return x;
+ }
+ int output(int a,int b,int c,int large)
+ {
+   printf("%d is the largest of three numbers",large);
+ }
+ int main()
+ {
+   int a,b,c,large;
+   a=input();
+   b=input();
+   c=input();
+   large=cmp(a,b,c);
+   output(a,b,c,large);
+
+ }
