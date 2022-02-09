eb502e4b55ab3c618da6f1ae4ef5aa7f68dd15a7
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-09 15:49:07 +0000

    done the final code

---
+++ p3final.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int input_n()
+{
+  int x;
+  printf("enter the number\n");
+  scanf("%d",&x);
+  return x;
+}
+int sum_n(int n)
+{
+  int sum=0;
+  for(int i=1;i<n;i++)
+  {
+    sum=sum+i;
+  }
+  return sum;
+}
+void output(int n,int sum)
+{
+  for(int i=1;i<n;i++)
+  {
+    printf("%d+",i);
+
+  }
+  printf("%d=%d",n,sum);
+}
+int main()
+{
+  int n,sm;
+  n=input_n();
+  sm=sum_n(n);
+  output(n,sm);
+  return 0;
+}  
+
+
+
+ 
